1041d9f925381ee6d1d0f3b9eeca2e3e1ee1987c
tlepri <tlepri@rogers.com>
2018-01-11 20:22:46 -0500

    modified helloworld app

--- src/basics/Helloworld.java
+++ src/basics/Helloworld.java
@@ -3,6 +3,7 @@ package basics;
 public class Helloworld {
     public static void main(String[] args){
         System.out.println("Helloworld");
+        System.out.println("I'm adding code to my project");
     }
 
 }
